2880487741f8ea611d5e43f9a8cc48eb1e79cd31
RamIowaStateUniversity <rramu@iastate.edu>
2016-01-31 15:38:21 -0600

    assign and try

--- boa/test/Main.java
+++ boa/test/Main.java
@@ -14,13 +14,18 @@ public class Main {
 	}	
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int x=sc.nextInt();
-		if(x>100) {
-			System.out.println(91);		
-		}	
-		else {
-			System.out.println(compute(x));		
+		try {		
+			Scanner sc=new Scanner(System.in);
+			int x=sc.nextInt();
+			x=90;
+			if(x>100) {
+				System.out.println(91);		
+			}	
+			else {
+				System.out.println(compute(x));		
+			}
+		}
+		catch(Exception e) {
 		}
 	}
 }
